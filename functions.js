de0b4ab7a0b10c91a1b8e119f33a134ca52e9cfc
2023MatthewSant <69405764+TheExplosion3@users.noreply.github.com>
2022-05-31 15:58:22 +0000

    sfd

--- functions.js
+++ functions.js
@@ -26,7 +26,7 @@ function observer() {
   let msgid_creditchange = new Map();
   // these are literally black magic do not edit (regex is hard)
   const posregex = /\b((John )?Cena)+|\b(Glorious)|\b(People'?s ?Republic ?Of ?China)|\b(All ?Hail)|\b(P\.?R\.?O\.?C\.?)|\b(C\.?C\.?P\.?)|\b(Collectivis(tic|t|m){1})|\b(Communis(tic|t|m){1})|\b(Mao( Zedong)?)|\b(Socialis(tic|t|m){1})|\b(Xi( ?Jinping)?)|\b(Little ?Red ?Book)|\b(Communist ?Manifesto)|\b(Xiaomi)|\b(Leader)/gi
-  const negregex = /\b((?! ?of)(\3 ?Of ?\4)|((The)? ?United ?States)|(Americas?))|\b(U\.?\.S\.?A?\.?)|\b((The)? ?West(ern(ers)?)?)|\b(Trade(r)?s?)|\b(Tianenmen( ?Square( ?Incident(( ?Of)? ?1989| ?1989)?)?)?)|\b(Capital(ist(ivism)?|ism))|\b((Jos(ef|eph))? ?Stalin(is(t|m)?)?)|\b(((C|K)arl)? ?Marx(is(t|m)?)?)|\b(Free((ness|dom)?( ?of ?(Speech|Rights))?)?)|\b(Re(bel(lions?)?|volution(ar(ies|ys?)|s)?|sistances?))|\b(Democra(t|tic(ness)?|c(ies|ys?)))|\b((Democratic)? ?Republic ?Of ?China)|\b(Taiwan(ese(ness)?|ness)?)|\b(Tibet(ans?|ese(ness)?|ness))|\b(U\.?S\.?S\.?R\.?|C\.?C\.?C\.?P|R\.?S\.?F\.?S\.?R\.?|E\.?U\.?|N\.?A\.?T\.?O\.?)|\b(European Union)|\b((The)?Soviet ?Union)|\b(North ?Atlantic ?Treaty ?Organization)|\b(Winnie ?the ?Pooh)|\b(Class(es|ist(ness|s)?)?)/gi
+  const negregex = /\b((?! ?of)(\3 ?Of ?\4)|((The)? ?United ?States)|(America(n?s?)?))|\b(U\.?\.S\.?A?\.?)|\b((The)? ?West(ern(ers)?)?)|\b(Trade(r)?s?)|\b(Tianenmen( ?Square( ?Incident(( ?Of)? ?1989| ?1989)?)?)?)|\b(Capital(ist(ivism)?|ism))|\b((Jos(ef|eph))? ?Stalin(is(t|m)?)?)|\b(((C|K)arl)? ?Marx(is(t|m)?)?)|\b(Free((ness|dom)?( ?of ?(Speech|Rights))?)?)|\b(Re(bel(lions?)?|volution(ar(ies|ys?)|s)?|sistances?))|\b(Democra(t|tic(ness)?|c(ies|ys?)))|\b((Democratic)? ?Republic ?Of ?China)|\b(Taiwan(ese(ness)?|ness)?)|\b(Tibet(ans?|ese(ness)?|ness))|\b(U\.?S\.?S\.?R\.?|C\.?C\.?C\.?P|R\.?S\.?F\.?S\.?R\.?|E\.?U\.?|N\.?A\.?T\.?O\.?)|\b(European Union)|\b((The)?Soviet ?Union)|\b(North ?Atlantic ?Treaty ?Organization)|\b(Winnie ?the ?Pooh)|\b(Class(es|ist(ness|s)?)?)/gi
   const modifieregex = /\\b(Glor((y|ious(ness)?)){1})|\b(Great(s|less(ness)?|ness)?)|\b((All ?-? ?)?(Power((less|ful)?(ness)?)))\b|\b(Wonderful)|\b((Unweak(e(r|n(ed|ings?){1}){1})?)|(Weak(lings?|e(ned|r|nings?){1}|ness(es)?)?))|\b((Un)?Smart(ness)?)|\b((Un)?Intellig(ences?|ents?)?)|\b((Un)?Wise(r|ly|ness)?)|\b(Stupids?(ity|ness)?)|\b(Horrible(s|ness)?)|\b(Idiot(ic|s)?)|\b(Bussing?s?)|\b(Clown(s|ing?)?)|\b(Poor(ness|er)?)|\b(Fail(s|ed|ings?|ures?)?)|\b(Los(s(es)?|t|ing|ers?){1})|\b(Belie(fs?|v(ing|es?)))|\b(Fault(s|ed)?)|\b(Pig(ness|s)?)|\b(Commie(s|ness)?)|\b(Bastard(ness|s)?)|\b(Lower(eds?|ness|s)?)|\b(Upper(s|ness|eds?)?)/gi
   
   collector.on('collect', m => {
@@ -34,35 +34,39 @@ function observer() {
     arr.push(m.content.author.id);
   });
 
+  // this could be improved with a better regex, but i don't really understand it to its full potential yet so I cant do so.
+
   collector.on('end', collected => {
-    let idx = 0;
-    let idxtwo = 0;
-    let previous_was_positive = false;
+    let ctr = 0;
+    let newVal = 0;
 
     if(collected !== null) {
       for(let m in collected) {
   
         const modstring = m;
         let currentarr;
-        
-        msgid_creditchange.set(arr[idx], (m.match(posregex).length - 1) * words.wordscore)
+        // changes social credit additively based upon num of pos words, increasing by 5 for each one
+        msgid_creditchange.set(arr[idx], (m.matchAll(posregex).length - 1) * words.wordscore)
         while(true) {
-          currentarr = m.match(posregex);
-          modstring = m.slice(idxtwo, m.search(posregex));
+          // defines the current array of matches to currentarr, modified string to modstring (sliced string down to final posregex match)
+          currentarr = m.matchAll(posregex);
+          modstring = m.slice(idxtwo, m.matchAll(posregex).length - 1);
           if(modstring.match(negregex) !== null) {
+            // sets wordscore based upon negative words, reducing if theyre found
+            msgid_credit.set(arr[idx], (msgid_credit.get(idx) - (modstring.match(negregex).length) - 1) * words.wordscore)
             
-            msgid_credit.set(arr[idx], msgid_credit.get(idx) - (modstring.match(negregex).length - 1))
-           
+            // if modifiers are found, foreach through all of them and change social credit accordingly.
             if(modstring.match(modifieregex) !== null) {     
-              let newVal = 0;
               currentarr = modstring.match(modifieregex);
               currentarr.forEach(str => {
                 if(words.modifiers.has(str)) {
-                  newVal += words.modifiers.str; 
+                  ctr++; 
+                  newVal += words.modifiers[str];
                 }
               });
               msgid_credit.set(arr[idx], msgid_credit.get(idx) + newVal);
             }
+            ctr++;
           }
           else if(modstring.match(modifieregex) == true) {
             if(true) {
@@ -72,15 +76,15 @@ function observer() {
           else {
             idx++;
             idxtwo = 0;
+          }
+          if(ctr === collected.length - 1) {
             break;
           }
         }
-        
-        msgid_creditchange.set(arr[idx], ((m.match(negregex).length - 1) * words.wordscore) * -1)
       }
     }
-    
-    bypass:
-      break bypass;
+    for(const [key, value] of msgid_creditchange.entries()) {
+      sc_change(true, value, key);
+    }
   });        
 }
